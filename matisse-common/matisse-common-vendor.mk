a9eacf2143f3032ab9cd86d13e234383eb671fb9
sub77 <sub77@ymail.com>
2017-02-01 00:25:25 +0000

    Revert "matisse-common: Fixing Camera2"
    This reverts commit c83aad06816e8b7f1472dc0e5d6dd9cf3c9caee0.

--- matisse-common/matisse-common-vendor.mk
+++ matisse-common/matisse-common-vendor.mk
@@ -326,8 +326,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/samsung/matisse-common/proprietary/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libdisp-aba.so:system/vendor/lib/libdisp-aba.so \
-    vendor/samsung/matisse-common/proprietary/vendor/lib/libmm-abl-oem.so:system/vendor/lib/libmm-abl-oem.so \
-    vendor/samsung/matisse-common/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
-    vendor/samsung/matisse-common/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libmm-abl-oem.so:system/vendor/lib/libmm-abl-oem.so
 
 $(call inherit-product, vendor/qcom/binaries/msm8974/graphics/graphics-vendor.mk)
